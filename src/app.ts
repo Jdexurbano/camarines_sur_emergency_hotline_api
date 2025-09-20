a1604f72f155a24dfdb3e82109f93b7f5d7f5bf3
Jdexurbano <dexterurbano25@gmail.com>
2025-09-21 00:37:08 +0800

    feat: Add new function for handling CORS
    Create a middleware for handling CORS  and  configure for dynamic CORS configuration
    
    -add new package "cors"
    -create middleware for CORS
    -add feature for dynamic CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express = require("express");
 import type { Request, Response } from "express";
 import api_router from "./routes/api_routes";
+import cors_middleware from "./middlewares/cors_middleware";
 
 const app = express();
+
+//use CORS middleware
+app.use(cors_middleware);
 
 app.use("/api", api_router);
 
